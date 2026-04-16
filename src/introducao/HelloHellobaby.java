b564d59fc844c9321868d824fd12e4e9e8e569ef
Flame <parasouzaf@gmail.com>
2026-04-16 17:01:46 -0300

    Iniciando o primeiro projeto java

---
+++ src/introducao/HelloHellobaby.java
@@ -0,0 +1,10 @@
+package introducao;
+
+public class HelloHellobaby {
+
+	public static void main(String[] args) {
+		System.out.print("Hello,Hello, baby");
+
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,9 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
